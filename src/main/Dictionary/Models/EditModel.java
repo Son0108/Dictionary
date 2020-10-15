79304538a3fb026a1298787e1049e0823e21559f
zero <vipgago9x@gmail.com>
2020-10-15 17:54:23 +0700

    edit fxml

--- src/main/Dictionary/Models/EditModel.java
+++
@@ -1,9 +0,0 @@
-package main.Dictionary.Models;
-
-public class EditModel {
-    public int EngListId;
-    public String Transcribe;
-    public int VietnameseId;
-    public String VietnameseWord;
-    public int WordTypeId;
-}
